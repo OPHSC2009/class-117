b419ef074c45850eccb2e1be3136816e3e2ac915
OPHSC2009 <92368269+OPHSC2009@users.noreply.github.com>
2023-05-16 23:24:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+
+}
+
+function setup()
+{
+    canvas=createCanvas(400,300);
+    canvas.center();
+    canvas.background("white")
+    
+}
+
+function draw(){
+
+}
+
+function clear_canvas(){
+
+}
